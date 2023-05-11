7db760f8c24c5e0f5a284ac4cc84658e1e07803c
MinhPhap1508 <minhphapclt@gmail.com>
2023-05-11 10:06:25 +0700

    baitap

--- Module_2/src/ss3/bai_tap/MergeArr.java
+++ Module_2/src/ss3/bai_tap/MergeArr.java
@@ -7,13 +7,12 @@ public class MergeArr {
         int[] arr1 = {2, 45, 65, 1, 8};
         int[] arr2 = {9, 865, 31, 5, 71, 77};
         int[] arr3 = new int[arr1.length + arr2.length];
-        for (int i = 0; i < arr3.length; i++) {
+        for (int i = 0; i < arr1.length; i++) {
             arr3[i] = arr1[i];
-            arr3[arr1.length]=arr2[i];
         }
-//        for (int i = arr1.length; i < arr3.length; i++) {
-//            arr3[i] = arr2[i - arr1.length];
-//        }
+        for (int i = arr1.length; i < arr3.length; i++) {
+            arr3[i] = arr2[i - arr1.length];
+        }
         System.out.print(Arrays.toString(arr3));
     }
 }
---
+++ Module_2/src/ss4/thuc_hanh/Rectangle.java
@@ -0,0 +1,36 @@
+package ss4.thuc_hanh;
+
+import java.util.Scanner;
+
+public class Rectangle {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the width: ");
+        double width = scanner.nextDouble();
+        System.out.print("Enter the height: ");
+        double height = scanner.nextDouble();
+        Rectangle rectangle = new Rectangle(width, height);
+        System.out.print("Your Rectangle \n" + rectangle.display());
+        System.out.println("Perimeter of the Rectangle: " + rectangle.getPerimeter());
+        System.out.println("Area of the Rectangle: " + rectangle.getArea());
+    }
+
+    double width, height;
+
+    public Rectangle(double width, double height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    public double getArea() {
+        return width * height;
+    }
+
+    public double getPerimeter() {
+        return (width + height) * 2;
+    }
+
+    public String display() {
+        return "Rectangle{" + "width=" + width + ", height=" + height + "}";
+    }
+}
