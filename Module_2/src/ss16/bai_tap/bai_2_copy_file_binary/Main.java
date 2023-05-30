6ad71f198b7f5b2115806b4925d4c5d93f0d8c04
MinhPhap1508 <minhphapclt.email@gmail.com>
2023-05-30 08:07:14 +0700

    bai tap

--- Module_2/src/ss16/bai_tap/bai_2_copy_file_binary/Main.java
+++ Module_2/src/ss16/bai_tap/bai_2_copy_file_binary/Main.java
@@ -2,51 +2,49 @@ package ss16.bai_tap.bai_2_copy_file_binary;
 
 import java.io.*;
 import java.util.Scanner;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
 
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Nhập file source");
-        String source = scanner.nextLine();
-        System.out.println("Nhập file target");
-        String target = scanner.next();
-        File file = new File(target);
+        Scanner in = new Scanner(System.in);
 
-        byte[] arr = readFile(source);
-        writeFile(target, arr);
-        System.out.println("Copy thành công");
-        System.out.println("Số byte trong tệp là: " + file.length());
+        System.out.println("Nhập source file:");
+        String sourcePath = in.nextLine();
+        System.out.println("Nhập target file:");
+        String destPath = in.nextLine();
+
+        File sourceFile = new File(sourcePath);
+        File destFile = new File(destPath);
+
+        try {
+            copyFileUsingJava7Files(sourceFile, destFile);
+            System.out.print("Copy thành công");
+        } catch (IOException ioe) {
+            System.out.print("Không thể copy ");
+            System.out.print(ioe.getMessage());
+        }
     }
 
-    public static void writeFile(String path, byte[] arr) {
-        File file = new File("src/ss16/bai_tap/bai_2_copy_file_binary/source.txt");
+    private static void copyFileUsingJava7Files(File source, File dest) throws IOException {
+        Files.copy(source.toPath(), dest.toPath(), StandardCopyOption.REPLACE_EXISTING);
+    }
+
+    private static void copyFileUsingStream(File source, File dest) throws IOException {
+        InputStream is = null;
+        OutputStream os = null;
         try {
-            OutputStream outputStream = new FileOutputStream(path);
-            for (int i = 0; i < file.length(); i++) {
-                byte b = arr[i];
-                outputStream.write(b);
+            is = new FileInputStream(source);
+            os = new FileOutputStream(dest);
+            byte[] buffer = new byte[1024];
+            int length;
+            while ((length = is.read(buffer)) > 0) {
+                os.write(buffer, 0, length);
             }
-            outputStream.close();
-        } catch (FileNotFoundException e) {
-            System.out.println("File không tồn tại");
-        } catch (IOException e) {
-            System.out.println("Error");
+        } finally {
+            is.close();
+            os.close();
         }
-    }
-
-    public static byte[] readFile(String path) {
-        try {
-            InputStream inputStream = new FileInputStream(path);
-            byte[] arr = new byte[1024];
-            inputStream.read(arr);
-            inputStream.close();
-            return arr;
-        } catch (FileNotFoundException e) {
-            System.out.println("File không tồn tại");
-        } catch (IOException e) {
-            System.out.println("Error");
-        }
-        return null;
     }
 }
